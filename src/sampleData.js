a11fd6959ff81e682708eb5db7133ab934d6f8ba
Ashish More <moreashishshivaji23@gmail.com>
2025-07-27 22:07:05 +0530

    firstupdate

--- src/sampleData.js
+++ src/sampleData.js
@@ -2,19 +2,19 @@ export const Tasks = [
     {
         id:1,
         todo:"Gym",
-        work:"Before giving up remember yourself why you started",
+        work:"Before giving up, remember why you started.",
         done: false
     },
     {
         id:2,
         todo:"Coding",
-        work:"Become a pro coder not a avearage",
+        work: "Become a pro coder, not just average.",
         done: false
     },
     {
         id:3,
         todo:"Kabaddi",
-        work:"A raider never stops at one point. Neither should a dreamer.",
+        work: "A raider never stops at one point. Neither should a dreamer.",
         done: false
     }
 ]
